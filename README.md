9a5a698ee9f88c021f90c93da174944305607956
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2023-05-04 19:22:41 -0700

    Added working pipeline

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # danlp-spacy-flair
-A spacy wrapper for DaNLPs flair model. We don't recommend using this for other than testing as better models are available.
+A spacy wrapper for DaNLPs flair model. It is a simple wrapper that adds the flair model as a spacy pipeline. It is not
+optimized for speed.
 
-We use it for validation using DaCy.
+We don't recommend using this for other than testing as better models are available. We use it for validation using DaCy.
+
 
 ## Installation
 ```bash
 pip install https://github.com/centre-for-humanities-computing/danlp-spacy-flair
+```
+
+## Usage
+```python
+import spacy
+from danlp_spacy_flair import DanlpFlairComponent  # just to register the component
+
+nlp = spacy.blank("da")
+nlp.add_pipe("danlp_flair", last=True)
+doc = nlp("Jeg hedder Anders og bor i Odense.")
+print(doc.ents)
+# (Anders, Odense)
 ```
--- src/danlp_spacy_flair/__init__.py
+++ src/danlp_spacy_flair/__init__.py
@@ -7,24 +7,26 @@ from spacy.language import Language
 from spacy.tokens import Doc
 
 
-@Language.component("danlp_flair_component")
-def my_component(nlp):
-    return FlairComponent(nlp=nlp)
+@Language.factory("danlp_flair")
+def my_component(nlp, name):
+    """A custom component that loads a Flair model and adds named entities and POS tags"""
+    return FlairComponent(nlp=nlp, name=name)
 
 
 class FlairComponent:
-    def __init__(self, nlp):
-        # self.name = name
+    def __init__(self, nlp, name):
+        self.name = name
         self.nlp = nlp
         # setup certificate to download flair
         ssl._create_default_https_context = ssl._create_unverified_context
 
+        # this also downloads the models
         self.tagger_ner = load_flair_ner_model()
         self.tagger_pos = load_flair_pos_model()
 
     def __call__(self, doc):
         sent = Sentence()
-        [sent.add_token(Token(t.text)) for t in self.nlp(doc.text)]
+        [sent.add_token(Token(t.text)) for t in doc]
         self.tagger_ner.predict([sent], verbose=True)
         self.tagger_pos.predict([sent], verbose=True)
 
@@ -40,11 +42,13 @@ class FlairComponent:
             ]
         )
         doc = Doc(self.nlp.vocab, words=text, spaces=ws, tags=upos, ents=iob)
+        return doc
 
 
 if __name__ == "__main__":
     import spacy
+
     nlp = spacy.blank("da")
-    nlp.add_pipe("danlp_flair_component", name="flair", last=True)
+    nlp.add_pipe("danlp_flair", name="flair", last=True)
     doc = nlp("Jeg hedder Anders og bor i Odense.")
     print(doc.ents)
